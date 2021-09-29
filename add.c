637485151f6e289cd625b6f398d3405010cff6d7
Harsh Pandey <akionsight@gmail.com>
2021-09-29 11:20:49 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter number A\n");
+    scanf("%d", &a);
+
+    printf("Enter number B \n");
+    scanf("%d", &b);
+
+    printf("your added number is %d\n", a+b);
+    return 0;
+}
